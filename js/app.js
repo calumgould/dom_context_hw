abc5af9cae664b5030d71e09a95d250f911b7409
calumgould <calumgould26@gmail.com>
2020-02-21 11:53:32 +0000

    app setup done

---
+++ js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newActorForm = document.querySelector('.new-actor-form');
+  newActorForm.addEventListener('submit', handleFormSubmit);
+});
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const actorListItem = createActorListItem(event.target);
+
+
+
+  const actorList = document.querySelector('.actor-list');
+  actorList.appendChild(actorListItem);
+
+
+  event.target.reset()
+};
+
+const createActorListItem = function (form) {
+
+  const actorListItem = document.createElement('li');
+  actorListItem.classList.add('actor-list-item');
+
+  const actorName = document.createElement('h2');
+  actorName.textContent = form.name.value;
+  actorListItem.appendChild(actorName);
+
+  const favouriteFilm = document.createElement('h3');
+  favouriteFilm.textContent = form.film.value;
+  actorListItem.appendChild(favouriteFilm);
+
+  const genderSelect = document.createElement('h4');
+  genderSelect.textContent = form.gender.value;
+  actorListItem.appendChild(genderSelect);
+
+  return actorListItem;
+};
+
+// GENERATE ELEMENT FUNCTION FOR REFACTOR //
+// const generateElement = (container, tag, content, classes = []) => {
+//   const element = document.createElement(tag);
+//   element.textContent = content;
+//
+//   classes.forEach((className) => {
+//     element.classList.add(className);
+//
+//     container.appendChild(element);
+//   });
+// };
